dc31a33289198cef869c1e72d0674b007965debc
wavemstudios <steve@wavemstudios.com>
2016-08-09 10:11:36 +0100

    push from eclipse

--- dukpt.c
+++ dukpt.c
@@ -26,7 +26,6 @@
  * arm-linux-gcc -Wall -Werror dukpt-demo.c -o dukpt-demo -lfepkcs11 -lcrypto
  * fesign --module opensc-pkcs11.so --pin 648219 --slotid 1 --keyid 00a0 \
  *	  --infile dukpt-demo
- * test again
  **/
 
 #include <stdio.h>
